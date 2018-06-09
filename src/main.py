9484fdc880739356828e77195b444fb807f2657b
sblayush <sblayush@gmail.com>
2018-06-09 17:21:38 +0530

    added main

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/'))
+sys.path.append(parent_dir)
+from src.web.app import start_app
+from src.utility.common_functions import does_dir_exist, create_dir
+import src.config.config_paths as config_paths
+
+
+if __name__ == "__main__":
+	if not does_dir_exist(config_paths.log_op_path):
+		create_dir(config_paths.log_op_path)
+	start_app()
