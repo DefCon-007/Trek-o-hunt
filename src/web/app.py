25571ba1f85ec75970ded3b5e053687ff40acc0e
sblayush <sblayush@gmail.com>
2018-06-10 09:34:34 +0530

    get images

--- src/web/app.py
+++ src/web/app.py
@@ -1,7 +1,7 @@
 import json
 import src.config.config_paths as config_paths
 from src.utility.common_functions import does_dir_exist, create_dir
-from flask import Flask, render_template, request, Response, session
+from flask import Flask, render_template, request, Response, session, send_from_directory
 from src.actions.login import login as action_login
 from src.actions.get_top_treks import get_top_treks as action_get_top_treks
 from src.actions.get_local_treks import get_local_treks as action_get_local_treks
@@ -12,7 +12,7 @@ from src.utility.get_logger import MyLogger
 
 logger = MyLogger.logger
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='images')
 app.secret_key = '102394873782491'
 
 
@@ -87,26 +87,15 @@ def save_images():
 		return Response("Successfully saved images", status=200)
 
 
-@app.route("/trekohunt/get_images", methods=["GET"])
-def get_images():
-	if 'file' not in request.files:
-		logger.info('No file part')
-		return Response("No file part",  status=100)
-	file = request.files['file']
-	if file.filename == '':
-		logger.info('No file selected!')
-		return Response("No file selected!", status=100)
-	if file:
-		filename = secure_filename(file.filename)
-		file.save(config_paths.images_path + "/" + filename)
-		logger.info("Successfully saved images")
-		return Response("Successfully saved images", status=200)
+@app.route("/images/<path:filename>", methods=["GET", "POST"])
+def get_images(filename):
+	return send_from_directory(directory='images', filename=filename)
 
 
 @app.route("/trekohunt/get_top_treks", methods=['GET'])
 def get_top_treks():
 	top_treks = action_get_top_treks()
-	top_treks = {'status': 200, "top_treks":top_treks}
+	top_treks = {'status': 200, "top_treks": top_treks}
 	if len(top_treks) > 0:
 		logger.info("Sending back: {}".format(top_treks))
 		return Response(json.dumps(top_treks), status=200, mimetype="application/json")
